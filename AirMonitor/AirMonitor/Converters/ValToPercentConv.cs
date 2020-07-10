94ac1bc0d39218155f7d03335989c3a6d6410150
kmyjak <kmyjak@wsei.edu.pl>
2020-07-10 23:51:49 +0200

    Added topercentCConverter

---
+++ AirMonitor/AirMonitor/Converters/ValToPercentConv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AirMonitor.Converters
+{
+    class ValToPercentConv : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!double.TryParse(value?.ToString(), out var result))
+            {
+                return value;
+            }
+            return result * 100;
+            //throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
