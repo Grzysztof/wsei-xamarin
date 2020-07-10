f9a3c06f597a3f4830a99d3653394c939b66e191
kmyjak <kmyjak@wsei.edu.pl>
2020-07-10 22:35:42 +0200

    HomeView, with model.

---
+++ AirMonitor/AirMonitor/ViewModels/HomeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using AirMonitor.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AirMonitor.ViewModels
+{
+    public class HomeViewModel
+    {
+        private INavigation _nav;
+        public HomeViewModel(INavigation nav)
+        {
+            _nav = nav;
+        }
+        private ICommand _goToDetails;
+        public ICommand GoToDetails => _goToDetails ?? (_goToDetails = new Command(OnGoToDetails));
+
+        private void OnGoToDetails()
+        {
+            _nav.PushAsync(new DetailsPage());
+        }
+
+    }
+}
